053ca06ba00cd38ff3bc8108956137b549d651b8
Stephen Kuenzli <skuenzli@qualimente.com>
2020-05-20 10:37:14 -0700

    Generate easy-to-load lists of s3 capabilities.

---
+++ k9policy/generate-capability-lists.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+filename=$1
+echo "generating access capability lists from ${filename}"
+
+access_capabilities='administer-resource use-resource read-data write-data delete-data'
+
+for access_capability in ${access_capabilities};
+do
+  grep "^S3\t${access_capability}" "${filename}" | grep "${access_capability}" | awk '{ print $3}' | tee "k9-access_capability.${access_capability}.tsv"
+done;
