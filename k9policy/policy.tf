45bb11c8c1a82dc3fb26c97b78027afcc1a01be9
Stephen Kuenzli <skuenzli@qualimente.com>
2020-05-20 16:23:15 -0700

    CM the rough cut access capability mappings.
    Need to introduce resource-type specific statements to get full coverage.

--- k9policy/policy.tf
+++ k9policy/policy.tf
@@ -1,8 +1,10 @@
 locals {
   # future work: retrieve action mappings from k9 api
-  actions_administer_resource = ["s3:*"]
+  actions_administer_resource_all = ["s3:*"]
+  actions_administer_resource_bucket = "${compact(split("\n", file("${path.module}/k9-access_capability.administer-resource.bucket.tsv")))}"
+  # actions_administer_resource_object = "${compact(split("\n", file("${path.module}/k9-access_capability.administer-resource.object.tsv")))}"
   actions_use_resource        = []
-  actions_read_data          = "${compact(split("\n", file("${path.module}/k9-access_capability.read-data.tsv")))}"
+  actions_read_data           = "${compact(split("\n", file("${path.module}/k9-access_capability.read-data.tsv")))}"
   actions_write_data          = "${compact(split("\n", file("${path.module}/k9-access_capability.write-data.tsv")))}"
   actions_delete_data         = "${compact(split("\n", file("${path.module}/k9-access_capability.delete-data.tsv")))}"
 }
@@ -11,7 +13,7 @@ data "aws_iam_policy_document" "bucket_policy" {
   statement {
     sid = "AllowRestrictedAdministerResource"
 
-    actions = "${local.actions_administer_resource}"
+    actions = "${local.actions_administer_resource_all}"
     resources = [
       "${var.s3_bucket_arn}",
     ]
@@ -73,6 +75,27 @@ data "aws_iam_policy_document" "bucket_policy" {
     condition {
       test     = "ArnEquals"
       values   = ["${var.allow_delete_data}"]
+      variable = "aws:PrincipalArn"
+    }
+  }
+
+  statement {
+    sid = "DenyEveryoneElse"
+
+    effect = "Deny"
+
+    actions = ["s3:*"]
+    resources = [
+      "${var.s3_bucket_arn}",
+      "${var.s3_bucket_arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "ArnNotEquals"
+      values   = ["${distinct(concat(var.allow_administer_resource, var.allow_read_data, var.allow_write_data, var.allow_delete_data))}"]
       variable = "aws:PrincipalArn"
     }
   }
