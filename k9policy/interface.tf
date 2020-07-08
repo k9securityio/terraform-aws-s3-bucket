59ea8be708f49f82d94add71eb2d927e63baf37f
Stephen Kuenzli <skuenzli@k9security.io>
2020-07-08 12:47:06 -0700

    Initial implementation of `AllowRestrictedCustomActions` statement.
    This works, but we need to hack through TF 0.11's inability to create a nested
    configuration block conditionally.  Ideally, the policy would not include a
    the `AllowRestrictedCustomActions` at all if no custom actions were specified.
    However, there is no way to express that directly in TF 0.11 syntax.
    
    Keep in mind that statements must refer to valid S3 actions, so cannot be
    something from `ec2:*` or `doesnotexist:*`.
    
    As a workaround, the `AllowRestrictedCustomActions` statement defaults to what
    is probably an innocuous action: `s3:GetAnalyticsConfiguration`.  The statement
    does not allow any principals access by default, so no access should be granted
    by the default configuration.

--- k9policy/interface.tf
+++ k9policy/interface.tf
@@ -51,6 +51,25 @@ variable "allow_delete_data_test" {
   description = "The IAM test to use in the policy statement condition, should be one of 'ArnEquals' (default) or 'ArnLike'"
 }
 
+variable "allow_custom_actions" {
+  type        = "list"
+  # the AllowRestrictedCustomActions statement needs a valid s3 action, so default to something innocuous: s3:GetAnalyticsConfiguration
+  default     = ["s3:GetAnalyticsConfiguration"]
+  description = "A custom list of S3 API actions to authorize ARNs listed in `allow_custom_actions_arns` to execute against this bucket."
+}
+
+variable "allow_custom_actions_arns" {
+  type        = "list"
+  default     = []
+  description = "The list of fully-qualified AWS IAM ARNs authorized to execute the custom actions against this bucket. Wildcards are supported. e.g. arn:aws:iam::12345678910:user/ci or arn:aws:iam::12345678910:role/app-backend-*"
+}
+
+variable "allow_custom_arns_test" {
+  type        = "string"
+  default     = "ArnEquals"
+  description = "The IAM test to use in the policy statement condition, should be one of 'ArnEquals' (default) or 'ArnLike'"
+}     
+
 output "policy_json" {
   value = "${data.aws_iam_policy_document.bucket_policy.json}"
 }
